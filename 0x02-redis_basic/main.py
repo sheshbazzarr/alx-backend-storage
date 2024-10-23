0d9ce06bbbb023be80db50b56a6e4db89cd6a1c4
sheshbazzarr <demerwdani@gmail.com>
2024-10-23 22:26:20 +0000

    update

---
+++ 0x02-redis_basic/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+import redis
+
+Cache = __import__('exercise').Cache
+
+cache = Cache()
+
+data = b"hello"
+key = cache.store(data)
+print(key)
+
+local_redis = redis.Redis()
