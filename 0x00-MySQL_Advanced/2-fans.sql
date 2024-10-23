f1055ebdaf42bd44672c438d582ff6103b094772
sheshbazzarr <demerwdani@gmail.com>
2024-10-23 09:43:36 +0000

    update

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,8 @@
 -- Task 2: 2 . best brand ever ! randks country origins of bands ,
 -- ordered by the number of (non-unique) fans
-SELECT origin, SUM(nb_fans) AS nb_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
+SELECT DISTINCT `origin`, SUM(`fans`) AS `nb_fans`
+FROM `metal_bands`
+GROUP BY `origin`
+ORDER BY `nb_fans` DESC;
+
 
